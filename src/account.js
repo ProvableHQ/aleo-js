841a28662693f0682baf93bcd728e92419088eaa
NContinanza <nicolas.continanza@entropy1729.com>
2022-10-26 18:58:39 -0300

    Implement decrypt, sign and verify functions

--- src/account.js
+++ src/account.js
@@ -18,6 +18,39 @@ class Account {
   address() {
     return this.adr;
   }
+
+  /**
+    * @param {string} ciphertext
+    * @returns {Record}
+    */
+  decryptRecord(ciphertext) {
+    return this.vk.decrypt(ciphertext)
+  }
+
+  /**
+    * @param {stringArray} ciphertexts
+    * @returns {Signature}
+    */
+  decryptRecords(ciphertexts) {
+    return ciphertexts.map(ciphertext => this.vk.decrypt(ciphertext))
+  }
+  
+  /**
+    * @param {Uint8Array} message
+    * @returns {Signature}
+    */  
+  sign(message) {
+    return this.pk.sign(message)
+  }
+
+  /**
+    * @param {Uint8Array} message
+    * @param {Signature} signature
+    * @returns {boolean}
+    */
+  verify(message, signature) {
+    return this.adr.verify(message, signature)
+  }
 }
 
 export default Account;
