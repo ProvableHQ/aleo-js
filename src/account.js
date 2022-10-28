a1427ee1c55cec419ea374565112f596a9539e95
Mati Onorato <monorato@forte.io>
2022-10-28 20:13:44 -0300

    fix docs

--- src/account.js
+++ src/account.js
@@ -1,16 +1,16 @@
 import { Address, PrivateKey, ViewKey } from "@entropy1729/aleo-sdk";
 /**
  * Class that represents an Aleo Account with a PrivateKey, from which an Address and a ViewKey derive.
+ * @example
+ * let account = new Account();
+ *
  */
 var Account = /** @class */ (function () {
     function Account(privatekey) {
         if (privatekey === void 0) { privatekey = ""; }
-        if (privatekey) {
-            this.pk = PrivateKey.from_string(privatekey);
-        }
-        else {
-            this.pk = new PrivateKey();
-        }
+        this.pk = privatekey
+            ? PrivateKey.from_string(privatekey)
+            : new PrivateKey();
         this.vk = ViewKey.from_private_key(this.pk);
         this.adr = Address.from_private_key(this.pk);
     }
@@ -34,6 +34,10 @@ var Account = /** @class */ (function () {
      * Decrypts a Record given a ciphertext.
      * @param {string} ciphertext
      * @returns {Record}
+     *
+     * @example
+     * let account = new Account();
+     * let record = account.decryptRecord("record1...");
      */
     Account.prototype.decryptRecord = function (ciphertext) {
         return this.vk.decrypt(ciphertext);
@@ -42,6 +46,10 @@ var Account = /** @class */ (function () {
      * Decrypts a set of Records given an array of ciphertexts.
      * @param {string[]} ciphertexts
      * @returns {Record[]}
+     *
+     * @example
+     * let account = new Account();
+     * let record = account.decryptRecords(["record1...", "record2..."]);
      */
     Account.prototype.decryptRecords = function (ciphertexts) {
         var _this = this;
@@ -53,6 +61,10 @@ var Account = /** @class */ (function () {
      *
      * @param {Uint8Array} message
      * @returns {Signature}
+     *
+     * @example
+     * let account = new Account();
+     * account.sign("a message");
      */
     Account.prototype.sign = function (message) {
         return this.pk.sign(message);
@@ -63,6 +75,12 @@ var Account = /** @class */ (function () {
      * @param {Uint8Array} message
      * @param {Signature} signature
      * @returns {boolean}
+     *
+     * @example
+     * let account = new Account();
+     * let message = "a message";
+     * let signature = account.sign(message);
+     * account.verify(message, signature);
      */
     Account.prototype.verify = function (message, signature) {
         return this.adr.verify(message, signature);
